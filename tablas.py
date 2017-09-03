81e313dffb272fb64e064e319dfd77e84ff8ef60
dcuda <31594495+dcuda@users.noreply.github.com>
2017-09-03 14:02:51 -0300

    Add files via upload

--- tablas.py
+++ tablas.py
@@ -1,10 +1,11 @@
 # -*- coding:utf-8 -*-
 
 # dado un nro natural imprimir su tabla de multiplicar desarrollada con sumas sucesivas
-
+# si se ingresa "0" no genera las sumas sucesivas
 nro = int(raw_input("Ingrese el numero: "))
-for j in range(1,11):
-	for i in range(1,j):
-			print str(nro),"+",
-	print str(nro),"=", nro*j
+if nro:
+	for j in range(1,11):
+		for i in range(1,j):
+				print str(nro),"+",
+		print str(nro),"=", nro*j
 	
